51de83a84e6822eaa940a33d65cd75248ea80e77
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-05-28 19:21:05 +0300

    Update mapManager.js

--- src/js/data/mapManager.js
+++ src/js/data/mapManager.js
@@ -1,6 +1,6 @@
 module.exports = {};
 
-const ctx = console.context();
+const ctx = console/**.context**/;
 
 if ("log" in ctx) {
   ctx.log("This script is made using Illya#9999's bundle processor, debundled by 0xffabc");
