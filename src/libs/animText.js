a2b4ad6f4f316f2b2ab424de6d1e2af6bc4c0319
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-05-30 20:23:02 +0500

    Update animText.js

--- src/libs/animText.js
+++ src/libs/animText.js
@@ -1,6 +1,6 @@
 module.exports.AnimText = function () {
   this.init = function (x, y, scale, speed, life, text, color) {
-    this.x = x, this.y = y, this.color = color, this.scale = scale, this.startScale = this.scale, this.maxScale = 1.5 * scale, this.scaleSpeed = 0.7, this.speed = speed, this.life = life, this.text = text;
+    this.x = x, this.y = y, this.color = color, this.scale = scale, this.startScale = this.scale, this.maxScale = 1.5 * scale, this.scaleSpeed = 0.1, this.speed = 0.05, this.life = life, this.text = text;
   }, this.update = function (delta) {
     this.life && (this.life -= delta, this.y -= this.speed * delta, this.scale += this.scaleSpeed * delta, this.scale >= this.maxScale ? (this.scale = this.maxScale, this.scaleSpeed *= -1) : this.scale <= this.startScale && (this.scale = this.startScale, this.scaleSpeed = 0), this.life <= 0 && (this.life = 0));
   }, this.render = function (ctxt, xOff, yOff) {
