948c41293d5b7cc7ed045523e8b3385d770d0f07
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-05 08:40:36 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -277,7 +277,7 @@ var useNativeResolution, showPing, delta, now, lastSent, attackState, player, pl
   ais = [],
   players = [],
   alliances = [],
-  gameObjects = new Map,
+  gameObjects = [],
   projectiles = [],
   projectileManager = new ProjectileManager(Projectile, projectiles, players, ais, objectManager, items, config, UTILS),
   aiManager = new AiManager(ais, AI, players, items, null, config, UTILS),
@@ -1022,7 +1022,7 @@ function killObjects(sid) {
 }
 
 function killObject(sid) {
-  const object = gameObjects.get(sid);
+  const object = gameObjects[sid];
   objectManager.disableBySid(sid);
   players.find(e => e.sid != player.sid && Math.hypot(player.x - e.x, player.y - e.y) < 180) && autoplace(object, true);
 }
@@ -1143,9 +1143,8 @@ function renderWaterBodies(xOffset, yOffset, ctxt, padding) {
 }
 
 function renderGameObjects(layer, xOffset, yOffset) {
-  let tmpX, tmpY, tmpObj, tmpSprite;
-  for (const [sid, object] of gameObjects)
-    (tmpObj = gameObjects.get(sid)?.active && (tmpX = tmpObj.x + tmpObj.xWiggle - xOffset, tmpY = tmpObj.y + tmpObj.yWiggle - yOffset, 0 == layer && tmpObj.update(delta), tmpObj.layer == layer && isOnScreen(tmpX, tmpY, tmpObj.scale + (tmpObj.blocker || 0)) && (mainContext.globalAlpha = tmpObj.hideFromEnemy ? 0.6 : 1, tmpObj.isItem ? (tmpSprite = getItemSprite(tmpObj), mainContext.save(), mainContext.translate(tmpX, tmpY), mainContext.rotate(tmpObj.dir), mainContext.drawImage(tmpSprite, -tmpSprite.width / 2, -tmpSprite.height / 2), tmpObj.blocker && (mainContext.strokeStyle = '#db6e6e', mainContext.globalAlpha = 0.3, mainContext.lineWidth = 6, renderCircle(0, 0, tmpObj.blocker, mainContext, !1, !0)), mainContext.restore()) : (tmpSprite = getResSprite(tmpObj), mainContext.drawImage(tmpSprite, tmpX - tmpSprite.width / 2, tmpY - tmpSprite.height / 2))));
+  for (var tmpSprite, tmpX, tmpY, i = 0; i < gameObjects.length; ++i)
+    (tmpObj = gameObjects[i])?.active && (tmpX = tmpObj.x + tmpObj.xWiggle - xOffset, tmpY = tmpObj.y + tmpObj.yWiggle - yOffset, 0 == layer && tmpObj.update(delta), tmpObj.layer == layer && isOnScreen(tmpX, tmpY, tmpObj.scale + (tmpObj.blocker || 0)) && (mainContext.globalAlpha = tmpObj.hideFromEnemy ? 0.6 : 1, tmpObj.isItem ? (tmpSprite = getItemSprite(tmpObj), mainContext.save(), mainContext.translate(tmpX, tmpY), mainContext.rotate(tmpObj.dir), mainContext.drawImage(tmpSprite, -tmpSprite.width / 2, -tmpSprite.height / 2), tmpObj.blocker && (mainContext.strokeStyle = '#db6e6e', mainContext.globalAlpha = 0.3, mainContext.lineWidth = 6, renderCircle(0, 0, tmpObj.blocker, mainContext, !1, !0)), mainContext.restore()) : (tmpSprite = getResSprite(tmpObj), mainContext.drawImage(tmpSprite, tmpX - tmpSprite.width / 2, tmpY - tmpSprite.height / 2))));
 }
 
 const speeds = [300, 400, 400, 300, 300, 700, 300, 100, 400, 600, 400, 1, 700, 230, 700, 1500];
@@ -1785,7 +1784,7 @@ let lastPing_ = Date.now();
 
 function updatePlayers(data) {
   queueMicrotask(() =>
-    nearestGameObjects = [...gameObjects].filter( object => object && Math.hypot(object?.x - player.x, object?.y - player.y) <= config.playerScale + (object?.group?.scale || 50) ));
+    nearestGameObjects = gameObjects.filter( object => object && Math.hypot(object?.x - player.x, object?.y - player.y) <= config.playerScale + (object?.group?.scale || 50) ));
   if (Date.now() - lastPing_ > 3000) {
     lastPing_ = Date.now();
     pingSocket();
