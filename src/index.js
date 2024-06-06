4f95ab6a1b07b4d7a98901e9096a12d29eec4c5d
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-06 09:15:01 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1202,7 +1202,7 @@ function gatherAnimation(sid, didHit, index) {
   if (sid != player.sid) return;
 
   const hitHat = breaking ? 40 : ((player.health < 100 && player.health > 60) ? 55 : 7);
-  const hitAcc = (player.health > 50) ? 15 : 18;
+  const hitAcc = (player.health > 50) ? 15 : (player.health < 40 ? 18 : 13);
   const idleHat = breaking ? 26 : (turretReload >= 2500 ? (turretReload = 0, 53) : 6);
   const idleAcc = players.length >= 2 ? 15 : (player.y <= config.snowBiomeTop ? 6 : 19);
 
