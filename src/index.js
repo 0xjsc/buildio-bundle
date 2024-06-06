20b2f3a128911f85eeb12b005453024555b1c4b3
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-06 18:33:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1901,6 +1901,7 @@ const modulesQueue = [
     tt && autoplace();
   }, (tt) => {
     if (breaking) return;
+    if (instakilling) return;
     
     if (tt?.skinIndex == 26 || tt?.skinIndex == 11) {
       io.send(packets.ATTACK, false, getAttackDir());
@@ -2014,7 +2015,7 @@ function render() {
       .fillStyle = '#dbc666', renderWaterBodies(xOffset, yOffset, mainContext, config.riverPadding), mainContext.fillStyle = '#91b2db', renderWaterBodies(
         xOffset, yOffset, mainContext, 250 * (waterMult - 1))), mainContext.lineWidth = 4, mainContext.strokeStyle = '#000', mainContext.globalAlpha = 0.06
     , mainContext.beginPath();
-  /*for (var i = -camX, k = -camY; i < maxScreenWidth || k < maxScreenHeight; i += gridDelta, k += gridDelta) {
+  for (var i = -camX, k = -camY; i < maxScreenWidth || k < maxScreenHeight; i += gridDelta, k += gridDelta) {
     if (i > 0) {
       mainContext.moveTo(i, 0);
       mainContext.lineTo(i, maxScreenHeight);
@@ -2023,7 +2024,7 @@ function render() {
       mainContext.moveTo(0, k);
       mainContext.lineTo(maxScreenWidth, k);
     }
-  }*/
+  }
   mainContext.stroke();
   mainContext.globalAlpha = 1;
   mainContext.strokeStyle = outlineColor;
