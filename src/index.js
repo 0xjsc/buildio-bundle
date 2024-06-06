8ca10ee7080a94351e8e035464884850d829c896
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-06 09:23:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1960,6 +1960,12 @@ var i = 0;
 
 function render() {
   now = Date.now(), delta = now - lastUpdate, lastUpdate = now;
+  /** 
+  * Smart trick to make every last frame lose their opacity by 50% 
+  * Which will implement motion blur  
+  **/
+  
+  mainContext.globalAlpha = 0.5; 
   if (player) {
     attackDir = UTILS.getDistance(camX, camY, player.x, player.y);
     tmp_Dir = UTILS.getDirection(player.x, player.y, camX, camY);
