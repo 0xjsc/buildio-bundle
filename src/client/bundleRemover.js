3cce84d2767cd1922a800013df6d941985e409e7
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-08 22:31:07 +0500

    Update bundleRemover.js

--- src/client/bundleRemover.js
+++ src/client/bundleRemover.js
@@ -1,4 +1,4 @@
-const regex = /bundle|assets|index|FRVR|howl|jquery|cookie|double|turnst/gm;
+const regex = /bundle|assets|index|FRVR|howl|jquery|cookie|double|turnst|frvr/gm;
 const doc = document.implementation.createHTMLDocument(document.title);
 
 doc.open();
