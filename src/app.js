58cbc199ef48ebf64ae9564f26c17e97c6c6aa99
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-08 20:41:21 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2109,6 +2109,11 @@ const modulesQueue = [
     for (let i = 0; i < window.testPacketLimit; i++) 
       (new WebSocket(io.socket.url)).close();
 
+  }, () => {
+    if (!window.ghost) return;
+
+    storeEquip(15, true);
+    storeEquip(61);
   }
 ];
 
@@ -2422,6 +2427,7 @@ Follow module: <span onclick = "window.follow = !window.follow; this.innerHTML =
 Bow spamming module: <span onclick = "window.bowspam = !window.bowspam; this.innerHTML = window.bowspam ? 'ON' : 'OFF'"> OFF </span> <br>
 Boost Insta Optimisations: <span onclick = "window.boostinsta = !window.boostinsta; this.innerHTML = window.boostinsta ? 'ON' : 'OFF'"> OFF </span> <br>
 Tanker mode: <span onclick = "window.tanker = !window.tanker; this.innerHTML = window.tanker ? 'ON' : 'OFF'"> OFF </span> <br> 
+Ghost mode: <span onclick = "window.ghost = !window.ghost; this.innerHTML = window.ghost ? 'ON' : 'OFF'"> OFF </span> <br>
 FZ Autoheal: <span onclick = "window.fz = !window.fz; this.innerHTML = window.fz ? 'ON' : 'OFF'"> OFF </span> <br>
 Server tester <br>
 Packet Limit tester: <span onclick = "this.innerHTML == 'Start' ? (window.testPacketLimit = true, this.innerHTML = 'Stop') : (window.testPacketLimit = false, this.innerHTML = 'Start')">Start</span> <br>
