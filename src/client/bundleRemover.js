e2a79c6937933dd0b1e4a9ea357a09450020efac
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-08 23:27:10 +0500

    Update bundleRemover.js

--- src/client/bundleRemover.js
+++ src/client/bundleRemover.js
@@ -1,3 +1,4 @@
+const { log } = console;
 const html = document.documentElement.innerHTML;
 const regex = /bundle|assets|index|FRVR|howl|jquery|cookie|double|turnst|frvr/gm;
 const doc = document.implementation.createHTMLDocument(document.title);
@@ -7,11 +8,13 @@ doc.write(html);
 doc.close();
 
 for (const element of doc.querySelectorAll("*[src]")) {
-  if (regex.test(element.src)) 
-    element.remove();
+  if (!regex.test(element.src)) continue;
+
+  log("[*] Removing element ", element);
+  element.remove();
 }
 
-document.replaceChild( document.importNode(doc.documentElement, true), document.documentElement);
+document.replaceChild(document.importNode(doc.documentElement, true), document.documentElement);
 
 if (location.href.includes("moomoo")) {
   window.turnstile.remove();
