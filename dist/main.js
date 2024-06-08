e08846901f0739e183fc32398c5ee0959f309f5f
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:21:51 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -2990,6 +2990,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+window.captchaCallback = () => connectSocketIfReady();
 document.body.replaceWith(document.body.cloneNode(true));
 
 const serverPackets = {};
