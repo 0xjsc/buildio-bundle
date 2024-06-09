78f09301bd819213909eded3c8ba4b04ded399e6
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 13:14:36 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -4037,7 +4037,7 @@ window.addEventListener('resize', _libs_utils_js__WEBPACK_IMPORTED_MODULE_2__["d
   aimOverride = false;
   setUsingTouch(!1), 1 != attackState && (attackState = 1, sendAtckState());
   touch = e.button == 0;
-  waka = touch ? player.weapons[0] : player.weapons[1];
+  waka = touch ? player.weapons[0] : (10 == player.weapons[1] ? 10 : player.weapons[0]);
 }, false), eventsListener.addEventListener('mouseup', function (e) {
   setUsingTouch(!1), 0 != attackState && (attackState = 0, sendAtckState());
 }, false);
@@ -5107,7 +5107,7 @@ const modulesQueue = [
     } else if (reloads[player.weapons[0]] == speeds[player.weapons[0]] && reloads[player.weapons[1]] != speeds[player.weapons[1]]) _libs_io_client_js__WEBPACK_IMPORTED_MODULE_1__["default"].send(packets.CHANGE_WEAPON, (waka = player.weapons[1]), true);
 
     if (reloads[player.weapons[1]] >= speeds[player.weapons[1]] && reloads[player.weapons[0]] >= speeds[player.weapons[0]] && player.weaponIndex != waka) {
-      waka = touch ? player.weapons[0] : player.weapons[1];
+      waka = touch ? player.weapons[0] : (10 == player.weapons[1] ? 10 : player.weapons[0]);
       _libs_io_client_js__WEBPACK_IMPORTED_MODULE_1__["default"].send(packets.CHANGE_WEAPON, waka, true);
     }
   },
