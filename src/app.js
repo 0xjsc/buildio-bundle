25771a00bbf44155068c2f49848eec869ab4159b
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-09 12:03:16 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2043,10 +2043,9 @@ const modulesQueue = [
     if (window.bowspam) return;
     if (breaking) return;
     
-    if (reloads[player.weapons[0]] !== speeds[player.weapons[0]] && player.weaponIndex != player.weapons[0]) {
+    if (reloads[player.weapons[0]] !== speeds[player.weapons[0]]) {
       io.send(packets.CHANGE_WEAPON, (waka = player.weapons[0]), true);
-    }
-    else if (reloads[player.weapons[0]] == speeds[player.weapons[0]] && reloads[player.weapons[1]] == speeds[player.weapons[1]] && player.weaponIndex != player.weapons[1]) io.send(packets.CHANGE_WEAPON, (waka = player.weapons[1]), true);
+    } else if (reloads[player.weapons[0]] == speeds[player.weapons[0]] && reloads[player.weapons[1]] != speeds[player.weapons[1]]) io.send(packets.CHANGE_WEAPON, (waka = player.weapons[1]), true);
 
     if (reloads[player.weapons[1]] >= speeds[player.weapons[1]] && reloads[player.weapons[0]] >= speeds[player.weapons[0]] && player.weaponIndex != player.weapons[0]) {
       io.send(packets.CHANGE_WEAPON, (waka = player.weapons[0]), true);
