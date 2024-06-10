4dbe617619a529ee6422a305eecec3785f440706
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 12:54:51 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -74,103 +74,103 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  "maxScreenWidth": 1920
-  , "maxScreenHeight": 1080
-  , "serverUpdateRate": 9
-  , "maxPlayers": 40
-  , "maxPlayersHard": 50
-  , "collisionDepth": 6
-  , "minimapRate": 3000
-  , "colGrid": 10
-  , "clientSendRate": 5
-  , "healthBarWidth": 50
-  , "healthBarPad": 4.5
-  , "iconPadding": 15
-  , "iconPad": 0.9
-  , "deathFadeout": 3000
-  , "crownIconScale": 60
-  , "crownPad": 35
-  , "chatCountdown": 3000
-  , "chatCooldown": 500
-  , "maxAge": 100
-  , "gatherAngle": 1.208304866765305
-  , "gatherWiggle": 10
-  , "hitReturnRatio": 0.25
-  , "hitAngle": 1.5707963267948966
-  , "playerScale": 35
-  , "playerSpeed": 0.0016
-  , "playerDecel": 0.993
-  , "nameY": 34
-  , "skinColors": ["#bf8f54", "#cbb091", "#896c4b", "#fadadc", "#ececec", "#c37373", "#4c4c4c", "#ecaff7", "#738cc3", "#8bc373"]
-  , "animalCount": 7
-  , "aiTurnRandom": 0.06
-  , "cowNames": ["Sid", "Steph", "Bmoe", "Romn", "Jononthecool", "Fiona", "Vince", "Nathan", "Nick", "Flappy", "Ronald", "Otis", "Pepe", "Mc Donald", "Theo",
+  "maxScreenWidth": 1920,
+  "maxScreenHeight": 1080,
+  "serverUpdateRate": 9,
+  "maxPlayers": 40,
+  "maxPlayersHard": 50,
+  "collisionDepth": 6,
+  "minimapRate": 3000,
+  "colGrid": 10,
+  "clientSendRate": 5,
+  "healthBarWidth": 50,
+  "healthBarPad": 4.5,
+  "iconPadding": 15,
+  "iconPad": 0.9,
+  "deathFadeout": 3000,
+  "crownIconScale": 60,
+  "crownPad": 35,
+  "chatCountdown": 3000,
+  "chatCooldown": 500,
+  "maxAge": 100,
+  "gatherAngle": 1.208304866765305,
+  "gatherWiggle": 10,
+  "hitReturnRatio": 0.25,
+  "hitAngle": 1.5707963267948966,
+  "playerScale": 35,
+  "playerSpeed": 0.0016,
+  "playerDecel": 0.993,
+  "nameY": 34,
+  "skinColors": ["#bf8f54", "#cbb091", "#896c4b", "#fadadc", "#ececec", "#c37373", "#4c4c4c", "#ecaff7", "#738cc3", "#8bc373"],
+  "animalCount": 7,
+  "aiTurnRandom": 0.06,
+  "cowNames": ["Sid", "Steph", "Bmoe", "Romn", "Jononthecool", "Fiona", "Vince", "Nathan", "Nick", "Flappy", "Ronald", "Otis", "Pepe", "Mc Donald", "Theo",
     "Fabz", "Oliver", "Jeff", "Jimmy", "Helena", "Reaper", "Ben", "Alan", "Naomi", "XYZ", "Clever", "Jeremy", "Mike", "Destined", "Stallion", "Allison",
     "Meaty", "Sophia", "Vaja", "Joey", "Pendy", "Murdoch", "Theo", "Jared", "July", "Sonia", "Mel", "Dexter", "Quinn", "Milky"
-  ]
-  , "shieldAngle": 1.0471975511965976
-  , "weaponVariants": [{
-    "id": 0
-    , "src": ""
-    , "xp": 0
-    , "val": 1
+  ],
+  "shieldAngle": 1.0471975511965976,
+  "weaponVariants": [{
+    "id": 0,
+    "src": "",
+    "xp": 0,
+    "val": 1
   }, {
-    "id": 1
-    , "src": "_g"
-    , "xp": 3000
-    , "val": 1.1
+    "id": 1,
+    "src": "_g",
+    "xp": 3000,
+    "val": 1.1
   }, {
-    "id": 2
-    , "src": "_d"
-    , "xp": 7000
-    , "val": 1.18
+    "id": 2,
+    "src": "_d",
+    "xp": 7000,
+    "val": 1.18
   }, {
-    "id": 3
-    , "src": "_r"
-    , "poison": true
-    , "xp": 12000
-    , "val": 1.18
+    "id": 3,
+    "src": "_r",
+    "poison": true,
+    "xp": 12000,
+    "val": 1.18
   }, {
-    "id": 4
-    , "src": "_e"
-    , "poison": true
-    , "xp": 15000
-    , "val": 1.21
-  }]
-  , "resourceTypes": ["wood", "food", "stone", "points"]
-  , "areaCount": 7
-  , "treesPerArea": 9
-  , "bushesPerArea": 3
-  , "totalRocks": 32
-  , "goldOres": 7
-  , "riverWidth": 724
-  , "riverPadding": 114
-  , "waterCurrent": 0.0011
-  , "waveSpeed": 0.0001
-  , "waveMax": 1.3
-  , "treeScales": [150, 160, 165, 175]
-  , "bushScales": [80, 85, 95]
-  , "rockScales": [80, 85, 90]
-  , "snowBiomeTop": 2400
-  , "snowSpeed": 0.75
-  , "maxNameLength": 15
-  , "mapScale": 14400
-  , "mapPingScale": 40
-  , "mapPingTime": 2200
-  , "volcanoScale": 320
-  , "innerVolcanoScale": 100
-  , "volcanoAnimalStrength": 2
-  , "volcanoAnimationDuration": 3200
-  , "volcanoAggressionRadius": 1440
-  , "volcanoAggressionPercentage": 0.2
-  , "volcanoDamagePerSecond": -1
-  , "volcanoLocationX": 13960
-  , "volcanoLocationY": 13960
-  , "MAX_ATTACK": 0.6
-  , "MAX_SPAWN_DELAY": 1
-  , "MAX_SPEED": 0.3
-  , "MAX_TURN_SPEED": 0.3
-  , "DAY_INTERVAL": 1440000
+    "id": 4,
+    "src": "_e",
+    "poison": true,
+    "xp": 15000,
+    "val": 1.21
+  }],
+  "resourceTypes": ["wood", "food", "stone", "points"],
+  "areaCount": 7,
+  "treesPerArea": 9,
+  "bushesPerArea": 3,
+  "totalRocks": 32,
+  "goldOres": 7,
+  "riverWidth": 724,
+  "riverPadding": 114,
+  "waterCurrent": 0.0011,
+  "waveSpeed": 0.0001,
+  "waveMax": 1.3,
+  "treeScales": [150, 160, 165, 175],
+  "bushScales": [80, 85, 95],
+  "rockScales": [80, 85, 90],
+  "snowBiomeTop": 2400,
+  "snowSpeed": 0.75,
+  "maxNameLength": 15,
+  "mapScale": 2000,
+  "mapPingScale": 40,
+  "mapPingTime": 2200,
+  "volcanoScale": 320,
+  "innerVolcanoScale": 100,
+  "volcanoAnimalStrength": 2,
+  "volcanoAnimationDuration": 3200,
+  "volcanoAggressionRadius": 1440,
+  "volcanoAggressionPercentage": 0.2,
+  "volcanoDamagePerSecond": -1,
+  "volcanoLocationX": 13960,
+  "volcanoLocationY": 13960,
+  "MAX_ATTACK": 0.6,
+  "MAX_SPAWN_DELAY": 1,
+  "MAX_SPEED": 0.3,
+  "MAX_TURN_SPEED": 0.3,
+  "DAY_INTERVAL": 1440000
 });
 
 
