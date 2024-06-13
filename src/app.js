8707c3f152562499374dca13219c1390226a5500
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-13 15:22:53 +0500

    Fixed disconnect

--- src/app.js
+++ src/app.js
@@ -140,7 +140,6 @@ if (location.href.includes("moomoo")) {
   }
 
 serverPackets[serverSide.INIT] = setInitData;
-serverPackets[serverSide.DISCONNECT] = disconnect;
 serverPackets[serverSide.SETUP_GAME] = setupGame;
 serverPackets[serverSide.ADD_PLAYER] = addPlayer;
 serverPackets[serverSide.REMOVE_PLAYER] = removePlayer;
