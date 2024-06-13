24d8fcfae17890ff95a3a881679be1a0e4239fda
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-13 13:48:40 +0500

    Update io-client.js

--- src/libs/io-client.js
+++ src/libs/io-client.js
@@ -8,31 +8,34 @@ const socket = {
   connect(address, callback, events) {
     if (this.socket) return;
     
-    this.socket = new WebSocket(address);
-    this.socket.binaryType = 'arraybuffer';
-    
-    this.socket.onmessage = message => {
-      let msg = new Uint8Array(message.data),
-        parsed = msgpack.decode(msg);
-      let [type, data] = parsed;
+    WebSocket = new Proxy(WebSocket, {
+      construct: (target, args) => {
+        this.socket = new target(args);
+        this.socket.addEventListener("message", message => {
+          let msg = new Uint8Array(message.data),
+          parsed = msgpack.decode(msg);
+          let [type, data] = parsed;
           
-      if (type == "io-init") this.socketId = data[0];
-      else if (events[type]) events[type].apply(void 0, data);
-    };
-    
-    this.socket.onopen = () => {
-      this.connected = true;
-      callback();
-    };
-    
-    this.socket.onclose = event => {
-      this.connected = false;
-      callback('Socket closed');
-    };
-    
-    this.socket.onerror = error => {
-      callback('Socket error');
-    };
+          if (type == "io-init") this.socketId = data[0];
+          else if (events[type]) events[type].apply(void 0, data);
+        });
+
+        this.socket.binaryType = 'arraybuffer';
+        this.socket.onopen = () => {
+          this.connected = true;
+          callback();
+        };
+        this.socket.onerror = error => {
+          callback('Socket error');
+        };
+        this.socket.onclose = event => {
+          this.connected = false;
+          callback('Socket closed');
+        };
+
+        return this.socket;
+      }
+    });
   },
   send(type) {
     const data = Array.prototype.slice.call(arguments, 1),
