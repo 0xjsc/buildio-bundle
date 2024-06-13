237466212eeffa763ddd6c2864aebd3200fd7567
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 10:41:46 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -3515,8 +3515,6 @@ const othersReloads  = [];
 function gatherAnimation(sid, didHit, index) {
   (tmpObj = findPlayerBySID(sid)) && tmpObj.startAnim(didHit, index);
   
-  if (sid == player.sid) reloads[waka] = 0;
-  else (othersReloads[tmpObj.sid] || (othersReloads[tmpObj.sid] = [0, 0]))[tmpObj.weaponIndex] = 0;
 }
 
 function loadGameObject(data) {
@@ -3595,15 +3593,10 @@ const modulesQueue = [
   }
 ];
 
-let attackDir = 0, tmp_Dir = 0, camSpd = 0;
-let lastPing_ = Date.now();
-
 function updatePlayers(data) {
   for (let i = 0; i < data.length;) {
     (tmpObj = findPlayerBySID(data[i])) && (tmpObj.t1 = void 0 === tmpObj.t2 ? tmpTime : tmpObj.t2, tmpObj.t2 = tmpTime, tmpObj.x1 = tmpObj.x, tmpObj.y1 = tmpObj.y, tmpObj.x2 = data[i + 1], tmpObj.y2 = data[i + 2], tmpObj.d1 = void 0 === tmpObj.d2 ? data[i + 3] : tmpObj.d2, tmpObj.d2 = data[i + 3], tmpObj.dt = 0, tmpObj.buildIndex = (tmpObj == player) ? -1 : data[i + 4], tmpObj.weaponIndex = data[i + 5], tmpObj.weaponVariant = data[i + 6], tmpObj.team = data[i + 7], tmpObj.isLeader = data[i + 8], tmpObj.skinIndex = data[i + 9], tmpObj.tailIndex = data[i + 10], tmpObj.iconIndex = data[i + 11], tmpObj.zIndex = data[i + 12], tmpObj.visible = !0), i += 13;
   }
-
-  if (!player?.health) return;
 
   /** ADD YOUR SCRIPTS IN MODULESQUEUE ONLY TO DON'T BREAK EVERYTHING **/
   
