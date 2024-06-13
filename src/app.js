1490d10fc3133c27f913c9e7992cb1eb3a75348e
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-13 14:15:07 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -952,7 +952,7 @@ const benchmarks = {
 const modulesQueue = [
   /** HELPER MODULES ARE GOING FIRST **/
   () => {
-    console.log(player, players, gameObjects);
+    console.log(player);
   }
 ];
 
