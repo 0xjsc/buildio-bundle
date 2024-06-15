cbfdb4639caf3d257ab8e573e4601b5883e1fc13
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 13:32:09 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -4789,6 +4789,8 @@ function gather(tmpObj) {
   const buildDamage = (_js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[tmpObj.weaponIndex].dmg * _config_js__WEBPACK_IMPORTED_MODULE_4__["default"].fetchVariant(tmpObj).val * 
         (_js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[tmpObj.weaponIndex].sDmg || 1) * 
         (tmpObj.skin && tmpObj.skin.bDmg ? tmpObj.skin.bDmg : 1)) || 0;
+
+  console.log(buildDamage);
   
   for (let i = 0; i < gameObjects.length; i++) {
     const obj = gameObjects[i];
