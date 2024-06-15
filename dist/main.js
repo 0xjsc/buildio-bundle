4c0988b12f3218129b216390e0a0762a1d0e42d5
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 13:03:04 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -5208,6 +5208,8 @@ function toAngles(objects) {
   return objects.map(object => Math.atan2(object.y - player.y2, object.x - player.x2));
 }
 
+let placers = [];
+
 function autoplace(enemy, replace = false) {
   if (instakilling) return;
   if (breaking) return;
