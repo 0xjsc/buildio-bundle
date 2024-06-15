da2e132ecccd74ef6f48142ad58bb8a3102aebc2
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 14:50:30 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -5281,8 +5281,8 @@ function autobreak(trap) {
   
   wsBridge.updateHittingState(true, trapAngle);
 
-  const buildDamage = (_js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[waka].dmg * _config_js__WEBPACK_IMPORTED_MODULE_4__["default"].fetchVariant(player).val * 
-        (_js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[waka].sDmg || 1) * 3.3) || 0;
+  const buildDamage = _js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[waka].dmg * _config_js__WEBPACK_IMPORTED_MODULE_4__["default"].fetchVariant(player).val * 
+        _js_data_items_js__WEBPACK_IMPORTED_MODULE_6__["default"].weapons[waka].sDmg || 1;
 
   if (trap.health - buildDamage <= 0) {
     wsBridge.sendChat("AntiSP T");
