b7f185b0f3debb4eb9346c5cbcece09e7872afa6
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-16 11:24:15 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1703,7 +1703,7 @@ function autoplace(enemy, replace = false) {
   const enemyDir = Math.atan2((enemy || window.enemyDanger)?.y - player.y, (enemy || window.enemyDanger)?.x - player.x);
   placers = [...toAngles(preplacableObjects), ...freeAngles].map((angle, i, array) => {
     const preplace = i < preplacableObjects.length;
-    place(player.items[((preplace || replace) && Math.abs(enemyDir - angle) < Math.PI / 2) ? 2 : (((Math.abs(angle - getMoveDir()) <= Math.PI / 2) && distance < items.weapons[player.weaponIndex].range + config.playerScale) ? 2 : 4)], angle);
+    place(player.items[((preplace || replace) && Math.abs(angle - getMoveDir()) > Math.PI && Math.abs(enemyDir - angle) < Math.PI / 2) ? 2 : (((Math.abs(angle - getMoveDir()) <= Math.PI / 2) && distance < items.weapons[player.weaponIndex].range + config.playerScale) ? 2 : 4)], angle);
     benchmarks.Placers += 3;
 
     return {
