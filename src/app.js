e5ce7cb7ca5bfeed96c45cbd9fa308e7fb6c0fbf
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-19 14:36:27 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2306,7 +2306,7 @@ var lastPing = -1;
 
 function pingSocketResponse() {
   var pingTime = Date.now() - lastPing;
-  window.pingTime = pingTime, pingDisplay.innerText = 'Ping: ' + pingTime + '\xA0ms';
+  window.pingTime = pingTime;
 }
 
 function pingSocket() {
