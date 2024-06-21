b49efc34db1bdedaafb2e53f789f8c5c3c148603
pankak <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 10:35:49 +0000

    bye bye :mewing:

--- dist/main.js
+++ dist/main.js
@@ -3828,7 +3828,7 @@ async function connectSocketIfReady() {
   const prefix = location.href.includes("moomoo") ? "re:" : "";
 
   log("[*] Grecaptcha resetted");
-  grecaptcha.reset();
+  // grecaptcha.reset();
   
   connectSocket(prefix + token, server);
 }
