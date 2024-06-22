a43053de302747ba98f659dac358d255dc31bb62
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-06-22 13:35:12 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2380,7 +2380,7 @@ function render() {
       .fillStyle = '#dbc666', renderWaterBodies(xOffset, yOffset, mainContext, config.riverPadding), mainContext.fillStyle = '#91b2db', renderWaterBodies(
         xOffset, yOffset, mainContext, 250 * (waterMult - 1))), mainContext.lineWidth = 4, mainContext.strokeStyle = '#000', mainContext.globalAlpha = 0.06
     , mainContext.beginPath();
-  for (var i = -camX, k = -camY; i < maxScreenWidth || k < maxScreenHeight; i += gridDelta, k += gridDelta) {
+  /*for (var i = -camX, k = -camY; i < maxScreenWidth || k < maxScreenHeight; i += gridDelta, k += gridDelta) {
     if (i > 0) {
       mainContext.moveTo(i, 0);
       mainContext.lineTo(i, maxScreenHeight);
@@ -2390,7 +2390,7 @@ function render() {
       mainContext.lineTo(maxScreenWidth, k);
     }
   }
-  mainContext.stroke();
+  mainContext.stroke();*/
   mainContext.globalAlpha = 1;
   mainContext.strokeStyle = outlineColor;
   renderGameObjects(-1, xOffset, yOffset);
